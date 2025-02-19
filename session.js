72e61aaa5cffbf327eb537eaae85640a5e4274be
SR436231 <dkdpc2008@gmail.com>
2025-02-19 03:13:48 -0800

    Update session.js

--- session.js
+++ session.js
@@ -25,7 +25,7 @@
 //*
 //Base by Janith Rashmika
 //GitHub: @Mrrashmika
-//WhatsApp: +94717775628
+//WhatsApp: +94770305962
 //Want more free bot scripts? Subscribe to my YouTube channel: https://youtube.com/@gamingrash2006
 //   * Created By GitHub: Mrrashmika
 //   * Credit To Janith Rashmika
@@ -40,6 +40,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "Your session id here",
+SESSION_ID: process.env.SESSION_ID || "ANJU-XPRO~TMlkXDiJ#isgPdCzkCi4Hf__C0JWk4eYyoT9jbZVllmvjx-uVA5s",
 PORT: process.env.PORT || "8000"
 };
